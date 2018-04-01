2f89664244c533872b483ea865d483382e516b47
Tim Goes <Tim_Goes4@hotmail.nl>
2018-04-01 14:08:54 +0200

    Small update to loanbrokerappgateway

--- application forms/loan broker/gateway/LoanBrokerAppGateway.java
+++ application forms/loan broker/gateway/LoanBrokerAppGateway.java
@@ -1,4 +1,25 @@
 package gateway;
 
+import model.gateways.MessageReceiverGateway;
+import model.gateways.MessageSenderGateway;
+import model.loan.LoanRequest;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
 public class LoanBrokerAppGateway {
+    private MessageSenderGateway sender;
+    private MessageReceiverGateway receiverGateway;
+    private LoanSerializer loanSerializer;
+
+    public LoanBrokerAppGateway(String senderChannel, String receiverChannel) throws JMSException, NamingException {
+        this.sender = new MessageSenderGateway(senderChannel);
+        this.loanSerializer = new LoanSerializer();
+        this.receiverGateway = new MessageReceiverGateway(receiverChannel);
+    }
+    
+    public void applyForLoan(LoanRequest request){
+
+    }
+
 }
